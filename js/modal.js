a4263d2df23b9cbb8723a6eb880edb146d33173f
vkadi-budetak <126949497+vkadi-budetak@users.noreply.github.com>
2025-11-10 22:14:31 +0100

    update modal

--- js/modal.js
+++ js/modal.js
@@ -2,17 +2,32 @@ const modalBtn = document.querySelector(".modal__button"); // получаем �
 
 const modal = document.querySelector(".modal"); // получаем модальное окно
 
+const modalClose = document.querySelector(".modal__close"); // получаем кнопку закрытия модального окна
+
 // вешаем обработчик события на кнопку
 modalBtn.addEventListener("click", () => {
   // при клике на кнопку
   modal.style.display = "flex"; // показываем модальное окно
 });
 
+// вешаем обработчик события на кнопку закрытия
+modalClose.addEventListener("click", () => {
+  modal.style.display = "none";
+});
+
+// вешаем обработчик события на модальное окно
 modal.addEventListener("click", (event) => {
   const modalContent = event.target.closest(".modal__inner"); // проверяем, был ли клик внутри содержимого модального окна
 
   if (!modalContent) {
-    modal.style.display = "";
+    modal.style.display = "none"; // если клик был вне содержимого, закрываем модальное окно
+  }
+});
+
+// вешаем обработчик события на документ для закрытия модального окна по нажатию клавиши Escape
+document.addEventListener("keydown", (event) => {
+  if (event.key === "Escape" && modal.style.display === "flex") {
+    modal.style.display = "none";
   }
 });
 // event - содержит информацию о событии
